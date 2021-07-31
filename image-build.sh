dad54fe2b1b03145a4ff6e6049ed0f02769f8df3
kylos101 <kylos101@gmail.com>
2021-07-31 13:58:27 -0400

    Iterate faster

--- image-build.sh
+++ image-build.sh
@@ -1,9 +1,14 @@
 # build our image
-docker build -t python-with-prometheus .
+IMAGE=$(docker build -q -t python-with-prometheus . | cut -d: -f2)
 
 # Uncomment me to test the image locally
 # docker run -p 8081:8000 -p 8080:5000 -it --rm --name slytherin-slimes python-with-prometheus
 
 # Prepare and push to a Docker Registry
-docker tag 8cd311638cfe kylos101/python-with-prometheus:0.0.16
-docker push kylos101/python-with-prometheus:0.0.16
+VERSION=0.0.17
+docker tag $IMAGE kylos101/python-with-prometheus:$VERSION
+docker push kylos101/python-with-prometheus:$VERSION
+
+# Use the new image
+sleep 3
+kubectl rollout restart deployments/python-with-prometheus
