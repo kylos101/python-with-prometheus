bf4277c46a21bb192b26418054072efd03c644ac
kylos101 <kylos101@gmail.com>
2021-07-31 15:37:02 -0400

    Descriptions on why each forward is helpful

--- port-forwards.sh
+++ port-forwards.sh
@@ -1,7 +1,12 @@
+# Prometheus UI
+# ...show the metrics
+# http://localhost:8082/graph?g0.expr=kubelet_running_pods&g0.tab=1&g0.stacked=0&g0.range_input=1h&g1.expr=running_pods&g1.tab=1&g1.stacked=0&g1.range_input=1h&g2.expr=app_hello_world_total&g2.tab=0&g2.stacked=0&g2.range_input=1h
 kubectl port-forward svc/metrics-prometheus-server 8082:80 &
-kubectl port-forward svc/slytherin-svc 5000:5000
-kubectl port-forward service/loki-stack-grafana 3000:80 &
 
-# Logging password
+# Inspect http://localhost:5000/metrics or call the API listening on http://localhost:5000
+kubectl port-forward svc/slytherin-svc 5000:5000
+
+# For debuggin'
+kubectl port-forward service/loki-stack-grafana 3000:80 &
 GRAFANA=$(kubectl get secret loki-stack-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)
 echo $GRAFANA
